b171b70ae74b5100dd6ba586e3755da0cb73a616
Pavlo Skobnikov <pavlo.skobnikov@gmail.com>
2024-09-04 17:29:29 +0300

    Update the setup shell script

--- setup.sh
+++ setup.sh
@@ -2,13 +2,16 @@
 
 # This script is used to setup any MacOS machine as my development environment
 #   w/ the help from Ansible.
+#
+# HINT: This script can be run w/o pulling the repository w/ the following
+# command: bash <(curl -s https://raw.githubusercontent.com/pavlo-skobnikov/ansible/main/setup.sh)
 
 # Firstly, let's install Homebrew to manage our packages.
 echo "Installing Brew..."
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew analytics off
 
-# Then, install to actually run our playbooks.
+# Then, install ansible to actually run our playbooks.
 brew install ansible
 
 # Lastly, pull down the Ansible playbook and run it.
